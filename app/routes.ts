7d241c842ff4289d77da05da124fdae5b5140102
jigangjon <jigangjon@gmail.com>
2025-11-28 14:24:37 +0900

    fixing routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import {
   type RouteConfig,
   index,
   layout,
+  prefix,
   route,
 } from "@react-router/dev/routes";
 
@@ -9,27 +10,27 @@ export default [
   layout("layouts/main.tsx", [
     index("routes/home.tsx"),
     route("login", "routes/login.tsx"),
-    route("admin", "routes/admin/dashboard.tsx", [
+    ...prefix("admin", [
       route("dashboard", "routes/admin/dashboard.tsx"),
       route("register", "routes/admin/register.tsx"),
     ]),
-    route("teacher", "routes/teacher/dashboard.tsx", [
+    ...prefix("teacher", [
       route("dashboard", "routes/teacher/dashboard.tsx"),
       route("register-class", "routes/teacher/register-class.tsx"),
-      route("school-admin", "routes/teacher/school-admin/dashboard.tsx", [
+      ...prefix("school-admin", [
         route("dashboard", "routes/teacher/school-admin/dashboard.tsx"),
         route("register", "routes/teacher/school-admin/register/index.tsx", [
           route(
             "teacher",
-            "routes/teacher/school-admin/register/register-teacher.tsx"
+            "routes/teacher/school-admin/register/register-teacher.tsx",
           ),
           route(
             "student",
-            "routes/teacher/school-admin/register/register-student.tsx"
+            "routes/teacher/school-admin/register/register-student.tsx",
           ),
           route(
             "room",
-            "routes/teacher/school-admin/register/register-room.tsx"
+            "routes/teacher/school-admin/register/register-room.tsx",
           ),
         ]),
       ]),
